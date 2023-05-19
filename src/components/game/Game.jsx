0f7a62e7683ad78a2428a8bdabe8c044907efcc9
Edluel <edluel_machado@hotmail.com>
2023-05-18 21:53:41 -0300

    feat: handle player choice

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import './game.css'
-import paper from '../../assets/images/icon-paper.svg'
-import rock from '../../assets/images/icon-rock.svg'
-import scissors from '../../assets/images/icon-scissors.svg'
-
+import React from 'react';
+import './game.css';
+import paper from '../../assets/images/icon-paper.svg';
+import rock from '../../assets/images/icon-rock.svg';
+import scissors from '../../assets/images/icon-scissors.svg';
 
 export default function Game(props) {
-  const {setChoice} = props;
+  const { setChoice } = props;
+
+  const handleChoice = (choice) => {
+    setChoice(choice);
+  };
 
   return (
     <div className="game">
       <div className="game-options">
         <div className='game-options-top'>
-          <div className="game-options-paper">
-            <img src={paper} className="game-options-svg" />
+          <div className="game-options-paper" onClick={() => handleChoice('paper')}>
+            <img src={paper} className="game-options-svg" alt="paper" />
           </div>
-          <div className="game-options-scissors">
-            <img src={scissors} className="game-options-svg" />
+          <div className="game-options-scissors" onClick={() => handleChoice('scissors')}>
+            <img src={scissors} className="game-options-svg" alt="scissors" />
           </div>
         </div>
         <div className='game-options-bot'>
-          <div className="game-options-rock">
-            <img src={rock} className="game-options-svg" />
+          <div className="game-options-rock" onClick={() => handleChoice('rock')}>
+            <img src={rock} className="game-options-svg" alt="rock" />
           </div>
         </div>
       </div>
     </div> 
-  )
+  );
 }
