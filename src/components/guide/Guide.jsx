97f6be3c8eba5ff2f29ab793f3034f845807beea
Edluel <edluel_machado@hotmail.com>
2023-05-25 15:09:22 -0300

    feat: rules board toggle function

--- src/components/guide/Guide.jsx
+++ src/components/guide/Guide.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './guide.css'
+import close from '../../assets/images/icon-close.svg'
+import rules from '../../assets/images/image-rules.svg'
 
 export default function Guide() {
-  return (
-    <div className="rules">
-        <button className='rules-bt'>RULES</button>
-    </div>
-  )
+    const [show, setShow] = useState(false);
+
+    const handleToggle = () => {
+        setShow(!show);
+    };
+
+    return (
+        <div className="rules">
+            <button className='rules-bt' onClick={handleToggle} >RULES</button>
+            {show && (
+                <div className="rules-screen">
+                    <div className="rules-screen-board">
+                        <div className="rules-screen-board-hearder">
+                            <h3>RULES</h3>
+                            <img src={close} onClick={handleToggle}/>
+                        </div>
+                        <div className="rules-screen-board-rules">
+                            <img src={rules} />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
 }
